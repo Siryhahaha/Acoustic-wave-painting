c1ddcc601bd79f035ad05b12fce38b9725b321d5
“Siryhahaha” <2052836615@qq.com>
2025-04-19 13:19:31 +0800

    截图程序改为bmp

--- Code/ScreenShot/ScreenShot.py
+++ Code/ScreenShot/ScreenShot.py
@@ -1,23 +1,20 @@
-#按下空格可以截图，保存.jpg文件在本目录下层jpg文件夹(esc退出)
+#按下空格可以截图，保存.bmp文件在本目录下层bmp文件夹(esc退出)
 
 import cv2
 
 cap = cv2.VideoCapture(0)
 
 prtsc_num = 1
-
 while cap.isOpened():
     success, image = cap.read()
-
     if not success:
         continue
-
-    cv2.imshow("SaveJpg", image)
+    cv2.imshow("Cap", image)
     key = cv2.waitKey(5) & 0xFF
     if key == 27:  # 按 ESC 退出
         break
     elif key == 32:
-        cv2.imwrite(f"jpg/prtsc{prtsc_num}.jpg", image)
+        cv2.imwrite(f"bmp/prtsc{prtsc_num}.bmp", image)
         prtsc_num = prtsc_num + 1
 
 cap.release()
